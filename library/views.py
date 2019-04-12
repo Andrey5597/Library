025a2c1708049183a4e4761bf2f2b23333681f92
Shapoval_Andrey <shapovalandrey27@gmail.com>
2019-04-12 16:32:30 +0300

    added documentation lines with queries, fixed problems with fields num_authors and num_genres

--- library/views.py
+++ library/views.py
@@ -3,19 +3,27 @@ from library.models import Book, BookInstance, BookDescription, BookSummary, Boo
 
 
 def home(request):
-
+    '''select count (book_title) from library_book;'''
     num_books = Book.objects.all().count()
+
+    '''select count (id) from library_BookInstance;'''
     num_instances = BookInstance.objects.all().count()
+
+    ''' select count (id) from library_BookInstance where status='a'; '''
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    #num_authors = BookDescription.objects.value('author_name').distinct().count()
-    #num_genres = BookDescription.objects.value('genre').distinct().count()
+
+    '''select count (distinct author_name) from library_BookDescription;'''
+    num_authors = BookDescription.objects.values('author_name').distinct().count()
+
+    '''select count (distinct genre) from library_BookDescription;'''
+    num_genres = BookDescription.objects.values('genre').distinct().count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
-         #'num_authors': num_authors,
-         #'num_genres': num_genres,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
     }
 
     return render(request, 'home.html', context=context)
